afd2e168a13a251a4f3318bbd31ea69fea063378
Sachin Kumar <sachin.kumar@infostride.com>
2022-01-31 13:20:08 +0530

    Day1Week6

--- com/Practice/AssignmnetWeek5.java
+++ com/Practice/AssignmnetWeek5.java
@@ -1,14 +1,30 @@
 package com.Practice;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AssignmnetWeek5 {
     ArrayList<String> collection = new ArrayList<>();
-    void collectData(){
-        collection.add("Stupid");
-        collection.add("Idiot");
-//        System.out.println(collection);
+    void collectData() throws IOException {
+        String path = System.getProperty("user.dir") + "/TestData/testData.xlsx";
+        //Reading the Excel file
+        FileInputStream file = new FileInputStream(path);
+        //Reading the workBook
+        XSSFWorkbook bk = new XSSFWorkbook(file);
+        //Reading the sheet
+        XSSFSheet sh = bk.getSheetAt(0);
+        //printing the no of coloum in the sheets
+        System.out.println(sh.getLastRowNum());
+        //Reading the file data using for loop
+        for(int i=0; i<=sh.getLastRowNum();i++) {
+            String word = sh.getRow(i).getCell(0).getStringCellValue();
+            collection.add(word);
+        }
     }
     public void check() {
         Scanner sc = new Scanner(System.in);
@@ -21,7 +37,7 @@ public class AssignmnetWeek5 {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         AssignmnetWeek5 obj = new AssignmnetWeek5();
         obj.collectData();
         obj.check();
